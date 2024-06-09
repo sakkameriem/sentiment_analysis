7e953e185b7e345b345d51f46e04d226c860c7dc
sakkameriem <131927303+sakkameriem@users.noreply.github.com>
2024-06-09 15:49:20 +0100

    Add files via upload

--- preprocessing.py
+++ preprocessing.py
@@ -1,17 +1,14 @@
 import pymongo
 import pandas as pd
 import re
-from IPython.display import display
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["mydatabase"]
-collection = db["comments_Zitouna"]
+db = client["Zitounadatabase"]
+collection = db["Raw scraped data"]
 
 def clean_text(text):
-    # remove special characters ( Arabic & French )
-    cleaned_text = re.sub(r'[^\w\sàâçéèêëîïôûùüÿæœÀÂÇÉÈÊËÎÏÔÛÙÜŸÆŒéàèùêûîôç]', ' ', text)
     # remove extra space
-    cleaned_text = re.sub(r'\s+', ' ', cleaned_text).strip().lower()
+    cleaned_text = re.sub(r'\s+', ' ', cleaned_text).strip().lower() 
     return cleaned_text
 
 
@@ -22,7 +19,7 @@ cleaned_df = Zitouna_df.dropna(subset=['text'])
 cleaned_df = cleaned_df.drop_duplicates(subset=['text'], keep='first')
 
 
-collection_cleaned = db["cleaned_comments_Zitouna"]
+collection_cleaned = db["Cleaned data"]
 collection_cleaned.insert_many(cleaned_df.to_dict(orient='records'))
 
 
